a7b7f5957469e550644612acf776af972215fbf0
sirine200 <sirinesoltanii123@gmail.com>
2025-02-05 00:01:47 +0100

    cookies setup

--- server/src/app.ts
+++ server/src/app.ts
@@ -58,9 +58,10 @@ app.use(
     saveUninitialized: false,
     store: MongoStore.create({ mongoUrl: process.env.MONGODB_URI! }),
     cookie: {
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-      maxAge: 24 * 60 * 60 * 1000, // 24 hours
+      secure: true, 
+      sameSite: "none", 
+      maxAge: 24 * 60 * 60 * 1000,
+      httpOnly: true
     },
   })
 );
